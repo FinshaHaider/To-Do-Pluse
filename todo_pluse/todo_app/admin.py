e1233b9a9479cd80b62f110fcab8e0564d387285
finsha haider <finshahaider@gmail.com>
2024-11-30 18:24:44 +0530

    updated

---
+++ todo_pluse/todo_app/admin.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from .models import Tasks,Todo,Users,Notes
+# Register your models here.
+
+
+
+admin.site.register(Tasks)
+admin.site.register(Todo)
+admin.site.register(Users)
+admin.site.register(Notes)
+
+
+
